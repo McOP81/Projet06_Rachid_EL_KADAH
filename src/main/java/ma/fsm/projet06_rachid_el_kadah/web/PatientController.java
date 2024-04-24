e23f0ad8e7031ca7756cc52ed9cbaa2401934b25
R-ELKADAH <rachidelkadah@email.com>
2024-04-24 15:41:06 +0100

    pagination commit

--- src/main/java/ma/fsm/projet06_rachid_el_kadah/web/PatientController.java
+++ src/main/java/ma/fsm/projet06_rachid_el_kadah/web/PatientController.java
@@ -19,10 +19,14 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
-                        @RequestParam(name = "size",defaultValue = "3") int size ){
-        Page<Patient> pagePatient = patientRepository.findAll(PageRequest.of(page,size));
+    public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int p,
+                        @RequestParam(name = "size",defaultValue = "3") int s,
+                        @RequestParam(name = "keyword",defaultValue = "") String kw ){
+        Page<Patient> pagePatient = patientRepository.findByNomContains(kw,PageRequest.of(p,s));
         model.addAttribute("listPatients", pagePatient.getContent());
+        model.addAttribute("pages",new int[pagePatient.getTotalPages()]);
+        model.addAttribute("currentPage",p);
+        model.addAttribute("keyword",kw);
         return "patients";
     }
 }

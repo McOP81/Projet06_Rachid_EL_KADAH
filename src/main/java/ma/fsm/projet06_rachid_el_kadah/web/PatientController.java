ab7f57ab451d1f1526930eef1792b562bf2b6294
R-ELKADAH <rachidelkadah@email.com>
2024-04-24 13:24:39 +0100

    third commit

--- src/main/java/ma/fsm/projet06_rachid_el_kadah/web/PatientController.java
+++ src/main/java/ma/fsm/projet06_rachid_el_kadah/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.fsm.projet06_rachid_el_kadah.web;
 import lombok.AllArgsConstructor;
 import ma.fsm.projet06_rachid_el_kadah.entities.Patient;
 import ma.fsm.projet06_rachid_el_kadah.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,10 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientList);
+    public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "3") int size ){
+        Page<Patient> pagePatient = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients", pagePatient.getContent());
         return "patients";
     }
 }
